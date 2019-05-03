08b91eaf017968a530207537a7440bc54bd0a2a6
KarkucinskiKrzysztof <ramsfeld.spam@gmail.com>
2019-05-03 16:46:44 +0200

    Addin Vets list to web

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,14 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import java.util.Set;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    String listVets(){
+    String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
