1e1a92e3ec443da323356c9a725c5efd41e66b85
KarkucinskiKrzysztof <ramsfeld.spam@gmail.com>
2019-05-14 22:08:38 +0200

    adding pet

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,16 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 /**
  * Created by jt on 7/25/18.
@@ -15,26 +20,54 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
+        owner1.setAddress("Morska 212/3");
+        owner1.setCity("Gdynia");
+        owner1.setTelephon(345363345);
         ownerService.save(owner1);
+
+        Pet michaelPet = new Pet();
+        michaelPet.setPetType(savedDogPetType);
+        michaelPet.setBirthDate(LocalDate.now());
+        michaelPet.setOwner(owner1);
+        michaelPet.setName("Pikop");
+        owner1.getPets().add(michaelPet);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
+        owner1.setAddress("Morska 212/3");
+        owner1.setCity("Gdynia");
+        owner1.setTelephon(345363345);
         ownerService.save(owner2);
+
+        Pet fionaPet = new Pet();
+        fionaPet.setPetType(savedCatPetType);
+        fionaPet.setBirthDate(LocalDate.of(2019,2,13));
+        fionaPet.setOwner(owner2);
+        fionaPet.setName("Czika");
+        owner1.getPets().add(fionaPet);
 
         System.out.println("Loaded Owners....");
 
